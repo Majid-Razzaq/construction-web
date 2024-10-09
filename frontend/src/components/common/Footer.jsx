c291edfbeaeaa43bd27f2d63af17faa127a14f6e
Majid-Razzaq <abdulmajid22770@gmail.com>
2024-10-09 23:35:08 +0500

    updated code

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+          <footer>
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-md-3">
+                    <h3>UrbanEdge Construction</h3>
+                    <div className="pe-5">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, voluptate neque iste</p>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                <h3 className="mb-3">Our Services</h3>
+                <ul>
+                    <li>
+                        <a href="">Specialty Construction</a>
+                    </li>
+                    <li>
+                        <a href="">Specialty Construction</a>
+                    </li>
+                    <li>
+                        <a href="">Specialty Construction</a>
+                    </li>
+                    <li>
+                        <a href="">Specialty Construction</a>
+                    </li>
+                </ul>
+                    
+                </div>
+
+                <div className="col-md-3">
+                <h3 className="mb-3">Quick Links</h3>
+                <ul>
+                    <li>
+                        <a href="">About Us</a>
+                    </li>
+                    <li>
+                        <a href="">Services</a>
+                    </li>
+                    <li>
+                        <a href="">Projects</a>
+                    </li>
+                    <li>
+                        <a href="">Contact us</a>
+                    </li>
+                </ul>
+
+                </div>
+
+                <div className="col-md-3">
+                <h3 className="mb-3">Contact Us</h3>
+                <ul>
+                    <li>
+                        <a href="">(1123-1231-12312)</a>
+                    </li>
+                    <li>
+                        <a href="">info@example.com</a>
+                    </li>
+                    <p>Address: Lorem ipsum dolor <br />sit amet consectetur <br />adipisicing elit.</p>
+                </ul>
+                </div>
+                <hr />
+                <div className='text-center pt-4'>Copyright @ 2024 UrbanEdge Constructions.All right reserved</div>
+            </div>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import AboutImg from '../../assets/images/about-us.jpg';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 
 const Home = () => {
    
   return (
     <>
-    <header>
-        <div className='container py-3'>
-            <Navbar expand="lg">
-                <Navbar.Brand href="#home" className='logo'><span>UrbanEdge</span> Construction</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
-                        <Nav.Link href="#link" className="nav-link">About Us</Nav.Link>
-                        <Nav.Link href="#link" className="nav-link">Services</Nav.Link>
-                        <Nav.Link href="#link" className="nav-link">Projects</Nav.Link>
-                        <Nav.Link href="#link" className="nav-link">Blogs</Nav.Link>
-                        <Nav.Link href="#link" className="nav-link">Contact Us</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-        </div>
 
-    </header>
+    <Header/>
     <main>
         {/* Hero section */}
         <section className='section-1'>
@@ -63,9 +46,7 @@ const Home = () => {
         </section>
     </main>
 
-    <footer>
-
-    </footer>
+    <Footer/>
     </>
   )
 }
