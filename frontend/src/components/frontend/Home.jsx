de9f94bf0818f5b61413da7c07450d4605a439e2
Majid-Razzaq <abdulmajid22770@gmail.com>
2024-10-12 10:09:24 +0500

    updated code

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import AboutImg from '../../assets/images/about-us.jpg';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import Serviceimg from '../../assets/images/construction1.jpg'
+import ServiceImg from '../../assets/images/construction1.jpg'
+import ConstructionImg from '../../assets/images/construction2.jpg'
+import Icon1 from '../../assets/images/icon-1.svg'
+import Icon2 from '../../assets/images/icon-1.svg'
+import Icon3 from '../../assets/images/icon-1.svg'
+
 
 const Home = () => {
    
@@ -55,24 +60,208 @@ const Home = () => {
                 <h2>our construction services</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
             </div>
-            <div className='row'>
+            <div className='row pt-4'>
                 <div className="col-md-3 col-lg-3">
                     <div className="item">
                         <div className="service-image">
-                            <img src={Serviceimg} className='w-100' alt="" />
+                            <img src={ServiceImg} className='w-100' alt="" />
                         </div>
                         <div className="service-body">
                             <div className="service-title">
                                 <h3>Specialty Construction</h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
                             </div>
                             <div className="service-content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium maxime, nulla expedita perspiciatis sit non, reprehenderit hic,</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium maxime, nulla</p>
                             </div>
                             <a href="" className='btn btn-primary'>Read More</a>
                         </div>
                     </div>
                 </div>
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ServiceImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                                <h3>Specialty Construction</h3>
+                            </div>
+                            <div className="service-content">
+                              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ServiceImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                                <h3>Specialty Construction</h3>
+                            </div>
+                            <div className="service-content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ServiceImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                                <h3>Specialty Construction</h3>
+                            </div>
+                            <div className="service-content">
+                              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    {/* Why Choose Us */}
+    <section className='section-4 py-5'>
+        <div className="container py-5">
+            <div className="section-header text-center">
+                <span>Why Choose Us?</span>
+                <h2>Discover our wide veriety of Project.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing <br />Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi est delectus ipsam, error ad quidem in blanditiis aliquam.</p>
+            </div> 
+            
+            <div className="row pt-4">
+                    <div className="col-md-4"> 
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon1} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solution</h3>
+                            </div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deserunt inventore tempore aut.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4"> 
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon2} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solution</h3>
+                            </div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deserunt inventore tempore aut.</p>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4"> 
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon3} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solution</h3>
+                            </div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam deserunt inventore tempore aut.</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    </section>
+
+        {/* Our Projects */}
+        <section className='section-3 bg-light py-5'>
+        <div className="container-fluid py-5">
+            <div className="section-header text-center">
+                <span>Our Projects</span>
+                <h2>Discover our diverse range of products</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+            </div>
+            <div className='row pt-4'>
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ConstructionImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                                <h3>Karachi Project</h3>
+                            </div>
+                            <div className="service-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium maxime, nulla</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ConstructionImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                            <h3>Karachi Project</h3>
+                            </div>
+                            <div className="service-content">
+                              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ConstructionImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                            <h3>Karachi Project</h3>
+                            </div>
+                            <div className="service-content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-3 col-lg-3">
+                    <div className="item">
+                        <div className="service-image">
+                            <img src={ConstructionImg} className='w-100' alt="" />
+                        </div>
+                        <div className="service-body">
+                            <div className="service-title">
+                            <h3>Karachi Project</h3>
+                            </div>
+                            <div className="service-content">
+                              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis nulla sint</p>
+                            </div>
+                            <a href="" className='btn btn-primary'>Read More</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
